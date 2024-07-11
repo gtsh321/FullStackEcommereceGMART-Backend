f9efc9876429834bb6dc10c7679ff8847064e85b
geetesh <gtsh321@gmail.com>
2024-07-11 23:55:17 +0530

    added the productCategory entity and created a relationship between the classes using manyToOne and OneToMany

--- src/main/java/com/gmart/entity/Product.java
+++ src/main/java/com/gmart/entity/Product.java
@@ -3,7 +3,18 @@ package com.gmart.entity;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -11,24 +22,42 @@ import lombok.Data;
 @Table(name = "product")
 @Data
 public class Product {
-
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
 	
+	@ManyToOne
+	@JoinColumn(name = "category_id", nullable = false)
+	private ProductCategory category;
+	
+	@Column(name = "sku")
 	private String sku;
 	
+	@Column(name = "name")
 	private String name;
 	
+	@Column(name = "description")
 	private String description;
 	
+	@Column(name = "unit_price")
 	private BigDecimal unitPrice;
 	
+	@Column(name = "image_url")
 	private String imageUrl;
 	
+	@Column(name = "active")
 	private boolean active;
 	
+	@Column(name = "units_in_stock")
 	private int unitsInStock;
 	
+	@Column(name = "date_created")
+	@CreationTimestamp
 	private Date dateCreated;
 	
+	@Column(name = "last_updated")
+	@UpdateTimestamp
 	private Date lastUpdated;
 }
