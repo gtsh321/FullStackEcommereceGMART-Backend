81c712fce83779e1eeb9025ec702a81da415599f
geetesh <gtsh321@gmail.com>
2025-02-08 15:34:57 +0530

    added the crossOrigin annotation

--- src/main/java/com/gmart/dao/ProductCategoryRepository.java
+++ src/main/java/com/gmart/dao/ProductCategoryRepository.java
@@ -2,10 +2,11 @@ package com.gmart.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.gmart.entity.ProductCategory;
 
-//name of json entry and the path for it 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- src/main/java/com/gmart/dao/ProductRepository.java
+++ src/main/java/com/gmart/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.gmart.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.gmart.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	
