d86d14f55dc8ff7f46ade28ddcc20448bffd237d
geetesh <gtsh321@gmail.com>
2025-02-08 13:46:42 +0530

    disable the other methods{PUT,DELETE,POST} made it read only

---
+++ src/main/java/com/gmart/config/MyDataRestConfig.java
@@ -0,0 +1,35 @@
+package com.gmart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.gmart.entity.Product;
+import com.gmart.entity.ProductCategory;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		// TODO Auto-generated method stub
+		RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+		
+
+		HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST };
+		
+		
+//		disable post,put,delete for product
+		config.getExposureConfiguration().forDomainType(Product.class)
+				.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+				.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+		
+//		disable post,put,delete for product-category
+		config.getExposureConfiguration().forDomainType(ProductCategory.class)
+				.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+				.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+	}
+
+}
