e226e9d4cfc76fbc0e54e8ab431750d004b67720
geetesh <gtsh321@gmail.com>
2025-02-21 19:55:48 +0530

    added a query method to fetch the products by category and exposed the ids for the product category in the restConfig.java

--- src/main/java/com/gmart/config/MyDataRestConfig.java
+++ src/main/java/com/gmart/config/MyDataRestConfig.java
@@ -1,5 +1,10 @@
 package com.gmart.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -9,8 +14,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import com.gmart.entity.Product;
 import com.gmart.entity.ProductCategory;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
+	
+	private EntityManager entityManager;
+	
+	@Autowired
+	public MyDataRestConfig(EntityManager theEntityManager)
+	{
+		entityManager = theEntityManager;
+	}
 
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -30,6 +46,29 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 		config.getExposureConfiguration().forDomainType(ProductCategory.class)
 				.withItemExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
 				.withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+		
+		//call an internal helper method to expose ids
+		exposeIds(config);
+	}
+
+	private void exposeIds(RepositoryRestConfiguration config) {
+		// TODO Auto-generated method stub
+		
+		//get a list of all entity classes from the entity manager
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+		
+		//create an array of the entity types
+		List<Class> entityClasses = new ArrayList<>();
+		
+		//get the entity types for the entities
+		for(EntityType tempEntityType : entities)
+		{
+			entityClasses.add(tempEntityType.getJavaType());
+		}
+		
+		//expose the entity ids for the array of entity/domain types
+		Class[] domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
 	}
 
 }
--- src/main/java/com/gmart/dao/ProductRepository.java
+++ src/main/java/com/gmart/dao/ProductRepository.java
@@ -1,6 +1,10 @@
 package com.gmart.dao;
 
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.gmart.entity.Product;
@@ -8,5 +12,6 @@ import com.gmart.entity.Product;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 	
 }
